6f38b01eeabbc7d16a404b39aec961020f8afdfb
MathPSantos <matheus.psantos2016@gmail.com>
2021-02-09 07:59:16 -0300

    feat: add styled-components

---
+++ app/src/styles/global.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * {
+    outline: 0;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    width: 100%;
+    background-color: #FF221F;
+    color: #FFFFFF;
+    font: 700 16px Ubuntu, Oxygen, sans-serif;
+    transition: all 0.5s;
+  }
+
+  #__next {
+    width: 100%;
+    min-height: 100vh;
+  }
+`
